fe1b74cb44acb1c06a6f792751d0bed0159b5f98
ArielAcarapi <acarapi.limachi51@gmail.com>
2025-11-29 08:13:27 -0400

    Adicione un boton para ir a mi repositorio en github

---
+++ script/script.js
@@ -0,0 +1,5 @@
+console.log("Bienvenido a mi portafolio");
+//alert("Gracias por visitar mi pagina");
+document.querySelector('h1').addEventListener('click', () => {
+    alert('Gracias por visitar mi pagina')
+});
